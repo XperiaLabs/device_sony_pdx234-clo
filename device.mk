189e34816ee04f6260e27e70b4e9a1146b21897a
Marc Bourgoin <themard85@gmail.com>
2024-07-17 21:35:37 -0600

    pdx234: Build vendor.qti.hardware.capabilityconfigstore from source
    Change-Id: I9bd58ddae3c1b3868db23dc44dfb80ad337e0e1b

--- device.mk
+++ device.mk
@@ -166,6 +166,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml \
     frameworks/native/data/etc/android.hardware.camera.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.xml
 
+# ConfigStore
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.capabilityconfigstore@1.0.vendor
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
